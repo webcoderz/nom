4dfc824d5ac5326c35753579e20ebe33dca20644
webcoderz <19884161+webcoderz@users.noreply.github.com>
2021-01-27 02:57:35 -0600

    psql initialization node functionality

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -21,8 +21,8 @@ mkdir -p /data/postgresdata
 chown postgres:postgres /data/postgresdata
 
 
-sudo -u postgres /usr/lib/postgresql/12/bin/initdb -D /data/postgresdata
-sudo -u postgres /usr/lib/postgresql/12/bin/pg_ctl -D /data/postgresdata -l logfile start
+#sudo -u postgres /usr/lib/postgresql/12/bin/initdb -D /data/postgresdata
+#sudo -u postgres /usr/lib/postgresql/12/bin/pg_ctl -D /data/postgresdata start
 
 
 sudo -u postgres psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='nominatim'" | grep -q 1 || sudo -u postgres createuser -s nominatim
@@ -30,6 +30,6 @@ sudo -u postgres psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='www-d
 sudo -u postgres psql postgres -c "DROP DATABASE IF EXISTS nominatim"
 useradd -m -p password1234 nominatim
 sudo -u nominatim /srv/nominatim/build/utils/setup.php --osm-file $NOMINATIM_DATA_PATH/$NOMINATIM_DATA_LABEL.osm.pbf --all --threads 2
-sudo -u nominatim ./srv/nominatim/build/utils/check_import_finished.php
-sudo -u postgres /usr/lib/postgresql/12/bin/pg_ctl -D /data/postgresdata stop
+#sudo -u nominatim ./srv/nominatim/build/utils/check_import_finished.php
+#sudo -u postgres /usr/lib/postgresql/12/bin/pg_ctl -D /data/postgresdata stop
 sudo chown -R postgres:postgres /data/postgresdata
